df77394de75382e4749febad8cf928c1b253bb83
Zhi Yong Tan <yongish@gmail.com>
2019-07-17 22:07:50 -0700

    Start accordion. Changed constrast to black on white.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Tab from 'react-bootstrap/Tab';
@@ -12,12 +15,14 @@ function App() {
       <header className="App-header">
         <Tabs defaultActiveKey="chinese" transition={false} id="noanim-tab-example">
           <Tab eventKey="chinese" title="Chinese">
+          
             <div style={{ display: 'flex' }}>
               <Form.Check inline type='radio' id='default-tab'
               label='Set as default tab.' defaultChecked />
               <FormControl type="text" placeholder="Search for word or pinyin"
               className="mr-sm-2" />
             </div>
+
             <div style={{ display: 'flex' }}>
               <div>
                 <h3 style={{ textDecoration: 'underline' }}>Familarity</h3>
@@ -36,9 +41,28 @@ function App() {
                   <Form.Check type='radio' id='display-english' label="English" />
                 </Form>
               </div>
-              <div>
-
-              </div>
+              <Accordion defaultActiveKey="0">
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                      Click me!
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>Hello! I'm the body aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="light" eventKey="1">
+                      Click me!
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>Hello! I'm another body</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
             </div>
           </Tab>
           <Tab eventKey="english" title="English (coming soon!)" disabled>
